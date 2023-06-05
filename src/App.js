0d508d260af821e67b6e8e2bc31276db78d84698
salzm <salmonzoor@gmail.com>
2023-06-05 11:36:10 -0400

    added to css and js files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Navbar } from "./components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
