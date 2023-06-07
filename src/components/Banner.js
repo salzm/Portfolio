8fa5911c8e1185debf0452571bd6e2cc48d5336b
salzm <salmonzoor@gmail.com>
2023-06-07 09:35:41 -0400

    made changes in banner js. Working on debugging why website is not showing up

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,48 @@
-import { Container, Row } from "react-bootstrap";
+import { userState, userEffect } from "react";
+import { Container, Form, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
 
 export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      ticker();
+    }, delta);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevdelta) => prevdelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
   return (
     <section className="banner" id="home">
       <Container>
@@ -9,12 +51,15 @@ export const Banner = () => {
             <span className="tagline">Welcome to my Portfolio!</span>
             <h1>
               {" "}
-              {`Hi I'm webcoded!`} <span className="wrap"> web developer </span>
+              {`Hi I'm webcoded!`} <span className="wrap"> {text} </span>
             </h1>
             <p> Test Test Test Test Test</p>
             <button onClick={() => console.log("connect")}>
-              Let's Connect!
+              Let's Connect! <ArrowRightCircle size={25}></ArrowRightCircle>
             </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Headder Img" />
           </Col>
         </Row>
       </Container>
